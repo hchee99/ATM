421b66cc971ece45e49744fadd0dc2f97b41db94
hchee99 <hchee99@naver.com>
2025-03-24 11:36:48 +0900

    [add]Step4

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI cashText;
+    public TextMeshProUGUI balanceText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Refresh();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Refresh()
     {
-        
+        if (GameManager.Instance != null)
+        {
+            nameText.text = GameManager.Instance.userData.userName;
+            cashText.text = GameManager.Instance.userData.cash.ToString();
+            balanceText.text = GameManager.Instance.userData.balance.ToString();
+        }
     }
 }
